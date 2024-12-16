0e5af4e8c0c61c09b4150c2a8e502d8fb7ce511e
andrelrocha <andrerocha0911@gmail.com>
2024-12-16 08:38:00 -0300

    feat: animação de slide up para content container

--- src/app/shared/content-container/content-container.component.ts
+++ src/app/shared/content-container/content-container.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-container',
   templateUrl: './content-container.component.html',
   styleUrls: ['./content-container.component.scss']
 })
-export class ContentContainerComponent {
+export class ContentContainerComponent implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.renderer.addClass(this.el.nativeElement, 'animate');
+    }, 100);
+  }
 }
