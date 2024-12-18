2ba6ee00d81428dd91f419dfa16b37c14ff1156a
andrelrocha <andrerocha0911@gmail.com>
2024-12-18 12:08:22 -0300

    feat: inputs no search form para destino e origem

--- src/app/shared/search-form/search-form.component.ts
+++ src/app/shared/search-form/search-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-form',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent {
-
+  toggleControl = new FormControl('round-trip');
 }
